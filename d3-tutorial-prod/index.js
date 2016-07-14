86c8d503e3fe4415e3f03092d97e20ea4ea69c20
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-07-14 16:17:08 -0400

    d3

--- d3-tutorial-prod/index.js
+++ d3-tutorial-prod/index.js
@@ -27,8 +27,13 @@ window.onload = function(){
   var svgCircles = d3.select("svg.circles");
   var svgMap = d3.select("svg.map").call(zoom);
 
+  var attributes = [{color: 'red', r: 40, cx: 100},  //example line
+                  {color: 'blue', r: 100, cx:19},
+                  {color: 'green', r: 30, cx:20},
+                  {color: 'yellow', r: 56, cx:230}];
+
   var circles = svgCircles.selectAll('circle')
-                          .data(attributesEnter)
+                          .data(attributes)
                           .enter()
                           .append("circle")
                           .attr('fill', color)
@@ -36,11 +41,35 @@ window.onload = function(){
                           .attr('cx', cx)
                           .attr('cy', '50px');
 
+
   // ADD THE attributesExit HERE! Remember to repeat at least one of the elements of attributesEnter
-  // var attributesExit = ;
-  //
-  // svgCircles.selectAll('circle')
-  //             .data(attributesExit)
-  //             .exit()
-  //             .attr("fill", changeColor);
+  var attributesExit = [{color: 'red', r: 40, cx: 100}];
+
+  svgCircles.selectAll('circle')
+              .data(attributesExit)
+              .exit()
+              .attr("fill", changeColor);
+
+  d3.json("usa.json", function(error, usa) {
+      if (error) return console.error(error);
+
+      var scale = 800;  // around 800 should be fine
+      var center = [39.5, 98.3];
+      var zoomOffset = 2;  // the amount the zoom center should deviate from the map's center
+
+      zoom.center(center.map(function(el){return el + zoomOffset;}));
+
+      var usaObject = usa.objects.layer1;
+      var topoUsaFeatures = topojson.feature(usa, usaObject);
+
+      var projectionLittle = d3.geo.mercator()
+                                .scale(scale)
+                                .center(center);
+
+      var path = d3.geo.path()
+                  .projection(projectionLittle);
+                  svgMap.append("path")
+                  .datum(topoUsaFeatures)
+                  .attr("d", path);
+    });
 };
